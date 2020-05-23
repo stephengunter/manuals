d06257e83502af69740687b3323a56095c5050d3
Stephen <stephen.chung@quantiumfund.com>
2020-05-23 15:42:25 +0800

    first commit

---
+++ src/Infrastructure/Views/BaseCategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Views
+{
+	public class BaseCategoryView : BaseRecordView
+	{
+		public string Title { get; set; }
+		
+		public int ParentId { get; set; }
+		public string ParentTitle { get; set; }
+	}
+}
